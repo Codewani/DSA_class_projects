dec3020391a5ce878da3a0eae6e4a036c66dc9be
Augustine Manu-Frimpong <150844417+FrimpsManu@users.noreply.github.com>
2025-01-24 01:36:32 +0000

    Adjusted the table format

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,10 @@
+#Arrays to store data
 students = []
 grades = []
 cartegories = []
 
-def cartegory(grade):
+#Function to categorize the grades
+def category(grade):
     if 90 <= grade <= 100:
         return "Excellent"
     elif grade >= 80:
@@ -12,28 +14,33 @@ def cartegory(grade):
     else:
         return "Needs Improvement"
 
+#Function to sadd students
 def add_student():
     if len(students) == 10:
         print("The system has reached the maximum number of students")
         return
-    name = input("Enter your name: ")
+    name = input("Enter the student's name: ")
     grade = int(input("Enter your grade: "))
 
     while not 0 <= grade <= 100:
-        print("Ensure your grade is between 0 and 100")
+        print("Error: Ensure your grade is between 0 and 100")
         grade = int(input("Enter your grade: "))
     
     students.append(name)
     grades.append(grade)
-    cartegories.append(cartegory(grade))
+    cartegories.append(category(grade))
 
 num_students = int(input("How many students do you want to add? "))
 
 for i in range(num_students):
     add_student()
-print("Name Grade category")
+
+#Adding the table format
+print('\nName        Grade       Category')
+print("----------------------------------")
+#print("Name      Grade       category")
 for i in range(len(students)):
-    print(f"{students[i]} {grades[i]} {cartegories[i]}")
+    print(f"{students[i]}    {grades[i]}    {cartegories[i]}")
 
 
 
