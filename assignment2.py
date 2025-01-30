60ee87166c448f2d46fb27bb0b26f50788ac01b5
FrancisGyamfi <francisot819@gmail.com>
2025-01-30 14:06:49 -0600

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,3 +1,49 @@
+item_name = []
+item_price = []
+
+def validation():
+    budget = 100
+
+    while budget > 0:
+        item = input("\nEnter item's name: ").strip()
+        try:
+            price = int(input("Enter item's price: "))
+            if price > budget:
+                print(f"\nYou don't have enough money left. Your remaining budget is {budget}$. Go to Cart.")
+                continue
+        except ValueError:
+            print("\nPlease enter a valid numeric value for the price.")
+            continue
+
+        budget -= price
+        item_name.append(item)
+        item_price.append(price)
+
+        print(f"\nYou have {budget}$ left in your wallet.")
+
+        if budget == 0:
+            print("\nYour budget is fully used up.")
+            break
+
+        prompt = input("\nDo you want to continue shopping (Yes/No): ").lower().strip()
+
+        while prompt != "yes"and prompt != "no":
+            print("\nEnter a valid response (Yes/No).")
+            prompt = input("\nDo you want to continue shopping (Yes/No): ").lower().strip()
+
+        if prompt == "no":
+            break
+
+    print("\nThank you for shopping with Walmart!")
+    print("\nItems Purchased:")
+    for i in range(len(item_name)):
+        print(f"{item_name[i]} - ${item_price[i]}")
+
+    print(f"\nFinal remaining budget: {budget}$")
+
+
+validation()
+
 def tax(price):
     try:
         return (0.1044 * price)
