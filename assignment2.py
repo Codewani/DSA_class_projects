db0be3b6868c64b9916024c4e2c8d2aa0c4701ee
Kondwani Mwape <59500285+Codewani@users.noreply.github.com>
2025-01-30 20:05:40 +0000

    tests

--- assignment2.py
+++ assignment2.py
@@ -8,7 +8,8 @@ def tax(price):
 def test_tax_function():
     assert tax("a") == -1
     assert tax("ben") == -1
-    assert float(tax(100)) == float(10.44)
-    print("all test cases passed")
+    assert round(tax(100), 2) == float(10.44)
+    assert round(tax(1000), 1) == float(104.4)
+    print("all test cases for tax function passed")
 
 test_tax_function()
