3acfffd0bfeefb060b0596749fdee29cb4098406
Kondwani Mwape <59500285+Codewani@users.noreply.github.com>
2025-01-30 13:53:36 -0600

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,6 +1,6 @@
 def tax(price):
     try:
-        return price + (0.1044 * price)
+        return (0.1044 * price)
     except:
         return -1
 
@@ -8,7 +8,7 @@ def tax(price):
 def test_tax_function():
     assert tax("a") == -1
     assert tax("ben") == -1
-    assert float(tax(100)) == float(110.44)
+    assert float(tax(100)) == float(10.44)
     print("all test cases passed")
 
 test_tax_function()
