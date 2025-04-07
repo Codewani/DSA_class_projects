c80d18cf67129cd29b8fec4335baf1103957499c
Augustine Manu-Frimpong <150844417+FrimpsManu@users.noreply.github.com>
2025-04-07 02:59:50 +0000

    Imported hashlib for password encryption

--- course_reg.py
+++ course_reg.py
@@ -1,4 +1,5 @@
 import csv
+import hashlib
 import os
 from typing import Set
 from datetime import datetime
@@ -47,8 +48,9 @@ class EnrollmentSystem:
         if student_id in self.students:
             print("Error: Student ID already exists")
             return False
-        self.students[student_id] = Student(student_id, name, password)
-        self.save_students()             # <-- ADD THIS LINE
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        self.students[student_id] = Student(student_id, name, hashed_password)
+        self.save_students()
         print(f"Successfully registered student: {name}")
         return True
 
@@ -198,7 +200,8 @@ def main():
                 password = input("Enter your password or 'E' to exit out of the Login page: ")
                 if password == 'E':
                     continue
-                while student_id not in system.students or password != system.students[student_id].password:
+                hashed_password = hashlib.sha256(password.encode()).hexdigest()
+                while student_id not in system.students or hashed_password != system.students[student_id].password:
                     print("The student_id and password combination you entered is not valid.")
                     student_id = input("Enter student ID or 'E' to exit out of the Login page: ")
                     if student_id == 'E':
@@ -206,11 +209,13 @@ def main():
                     password = input("Enter your password or 'E' to exit out of the Login page: ")
                     if password == 'E':
                         break
+                    hashed_password = hashlib.sha256(password.encode()).hexdigest()
                 
                 if student_id == 'E' or password == 'E':
                     continue
 
-                cur_student = student_id
+            cur_student = student_id
+
 
 
 
